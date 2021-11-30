0560549b702eaf876538c6ddb9840501e6bd6ba3
mrmagicalJ <1078787975@qq.com>
2021-11-30 22:49:05 +0800

    init

--- fil/api.js
+++ fil/api.js
@@ -3,11 +3,11 @@ const md5 = require('crypto-js/md5');
 const qs = require('qs')
 
 const baseParams = {
-    plat: 'web',
-    version: '1.0.0'
+  plat: 'web',
+  version: '1.0.0'
 }
 const headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
 }
 
 /**
@@ -16,47 +16,69 @@ const headers = {
  * @param {String} data.account 账号
  * @param {String} data.loginPwd 密码
  */
- const fetchLogin = data => axios({
-    url: 'http://fil.zooofx.com/app/customer/open/login',
-    method: 'post',
-    headers,
-    data: qs.stringify(data)
+const fetchLogin = data => axios({
+  url: 'http://fil.zooofx.com/app/customer/open/login',
+  method: 'post',
+  headers,
+  data: qs.stringify({ ...data, ...baseParams })
+})
+
+const fetchWeekList = params => axios({
+  url: 'http://fil.zooofx.com/app/signTask/weekList',
+  method: 'get',
+  headers,
+  params: {
+    ...params,
+    ...baseParams
+  }
 })
 
 const fetchDailyTask = params => axios({
-    url: 'http://fil.zooofx.com/app/signTask/dailyTask',
-    method: 'get',
-    headers,
-    params: {
-        ...params,
-        ...baseParams
-    }
+  url: 'http://fil.zooofx.com/app/signTask/dailyTask',
+  method: 'get',
+  headers,
+  params: {
+    ...params,
+    ...baseParams
+  }
 })
 
 const fetchTaskTopic = params => axios({
-    url: 'http://fil.zooofx.com/app/signTask/taskTopic',
-    method: 'get',
-    headers,
-    params: {
-        ...params,
-        ...baseParams
-    }
+  url: 'http://fil.zooofx.com/app/signTask/taskTopic',
+  method: 'get',
+  headers,
+  params: {
+    ...params,
+    ...baseParams
+  }
 })
 
 /**
  * 答题
  * @param {Object} data
  */
- const fetchSubmitAnswer = data => axios({
-    url: 'http://fil.zooofx.com/app/signTask/submitAnswer',
-    method: 'post',
-    headers,
-    data: qs.stringify({ ...data, ...baseParams })
+const fetchSubmitAnswer = data => axios({
+  url: 'http://fil.zooofx.com/app/signTask/submitAnswer',
+  method: 'post',
+  headers,
+  data: qs.stringify({ ...data, ...baseParams })
+})
+
+const fetchZeroLu = params => axios({
+  url: 'http://fil.zooofx.com/app/zeroLu/getConvertTipInfo',
+  method: 'get',
+  headers,
+  params: {
+    ...params,
+    ...baseParams
+  }
 })
 
 module.exports = {
-    fetchLogin,
-    fetchDailyTask,
-    fetchTaskTopic,
-    fetchSubmitAnswer
+  fetchLogin,
+  fetchWeekList,
+  fetchDailyTask,
+  fetchTaskTopic,
+  fetchSubmitAnswer,
+  fetchZeroLu
 }
--- fil/index.js
+++ fil/index.js
@@ -1,52 +1,83 @@
 const md5 = require('crypto-js/md5');
-const { fetchLogin, fetchDailyTask, fetchTaskTopic, fetchSubmitAnswer } = require('./api')
+const ProgressBar = require('progress');
+const { fetchLogin, fetchWeekList, fetchDailyTask, fetchTaskTopic, fetchSubmitAnswer, fetchZeroLu } = require('./api');
+
+const account = [
+	{ account: '13700015132', loginPwd: 'ypw153153' },
+	{ account: 'a0019912336161@163.com', loginPwd: 'ypw153153' },
+	{ account: 'a1519912336161@163.com', loginPwd: 'ypw153153' },
+]
 
 const dealRes = async (fn) => {
-    const { data: { code, result, msg } } = await fn;
-    if (code !== 200) throw new Error(msg);
-    return result
-}
+	const {
+		data: { code, result, msg },
+	} = await fn;
+	if (code !== 200) throw new Error(msg);
+	return result;
+};
 
 const login = async ({ account, loginPwd }) => {
-    try {
-        const { data: { code, result, msg } } = await fetchLogin({
-            account,
-            loginPwd: md5(loginPwd).toString(),
-            prefix: 86,
-            token: ''
-        });
-        if (code !== 200) throw new Error(msg);
-        return result.token
-    } catch (err) {
-        console.error(err);
-    }
-}
+	try {
+		const {
+			data: { code, result, msg },
+		} = await fetchLogin({
+			account,
+			loginPwd: md5(loginPwd).toString(),
+			prefix: 86,
+			token: '',
+		});
+		if (code !== 200) throw new Error(msg);
+		return result.token;
+	} catch (err) {
+		console.error(err);
+	}
+};
 
-const dailyTask = async () => {
-    try {
-        const { data: { code, result, msg } } = await fetchDailyTask({
-            token: '09fa6ff1894f4a7181586a90ab8acc04'
-        });
-        if (code !== 200) throw new Error(msg);
-    } catch (err) {
-        console.error(err);
-    }
+const task = async (account, loginPwd) => {
+	const total = 12;
+	try {
+		const token = await login({ account, loginPwd });
+		await dealRes(fetchZeroLu({ token }));
+		await dealRes(fetchWeekList({ token }));
+		await dealRes(fetchDailyTask({ token }));
+		const bar = new ProgressBar(`  ${account} [:bar] :percent`, {
+			complete: '=',
+			incomplete: ' ',
+			width: 20,
+			total,
+		});
+		for (let index = 0; index < total; index++) {
+			const { correctOption, topicId, topicType } = await dealRes(fetchTaskTopic({ token }));
+			await dealRes(fetchSubmitAnswer({ topicId, topicType, submitOption: correctOption, token }));
+			bar.tick(1);
+		}
+		console.log(`${account} 完成\n`);
+	} catch (error) {
+		console.error(error);
+	}
+	return;
 }
-
 
 const init = async () => {
-    const account = 13700015132;
-    const loginPwd = 'ypw153153';
-    try {
-        const token = await login({ account, loginPwd });
-        for (let index = 0; index < 13; index++) {
-            const { correctOption, topicId, topicType } = await dealRes(fetchTaskTopic({ token }))
-            await dealRes(fetchSubmitAnswer({ topicId, topicType, submitOption: correctOption, token }))
-        }
-        console.log(`${account} 完成`);
-    } catch (error) {
-        console.error(error);
-    }
-}
+	for (const { account, loginPwd } of account) {
+		await task(account, loginPwd)
+	}
+};
 
 init();
+
+// (async () => {
+//     const delay = () => new Promise((resolve, reject) => { setTimeout(resolve, 1000) })
+//     const account = 123123;
+//     const total = 13;
+//     const bar = new ProgressBar(`  ${account} [:bar] :percent`, {
+//         complete: '=',
+//         incomplete: ' ',
+//         width: 20,
+//         total,
+//       });
+//     for (let index = 0; index < total; index++) {
+//         await delay();
+//         bar.tick(1);
+//     }
+// })()
